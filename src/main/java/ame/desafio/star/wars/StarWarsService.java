dcc7ec7023ff577592273cba6b8322fc99b02476
Sandro Garani <sandro.garani@gmail.com>
2019-05-02 03:23:03 -0300

    Adicionado todas as funcionalidades desejadas

--- src/main/java/ame/desafio/star/wars/StarWarsService.java
+++ src/main/java/ame/desafio/star/wars/StarWarsService.java
@@ -4,40 +4,53 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import ame.desafio.star.wars.entity.Planeta;
 import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.MultiMap;
+import io.vertx.reactivex.core.buffer.Buffer;
 import io.vertx.reactivex.core.http.HttpServerResponse;
 import io.vertx.reactivex.ext.web.Router;
 import io.vertx.reactivex.ext.web.RoutingContext;
+import io.vertx.reactivex.ext.web.client.HttpRequest;
+import io.vertx.reactivex.ext.web.client.HttpResponse;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import io.vertx.reactivex.ext.web.codec.BodyCodec;
 import io.vertx.reactivex.ext.web.handler.BodyHandler;
 import io.vertx.reactivex.cassandra.CassandraClient;
 import io.vertx.reactivex.cassandra.CassandraRowStream;
 import io.vertx.reactivex.cassandra.Mapper;
 import io.vertx.reactivex.cassandra.MappingManager;
+import io.reactivex.Single;
 import io.vertx.cassandra.CassandraClientOptions;
+import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.WebClientOptions;
 
 public class StarWarsService extends AbstractVerticle {
 
-	CassandraClient cassandraClient;
+	private CassandraClient cassandraClient;
 
 	@Override
 	public void start() {
 
 		CassandraClientOptions options = new CassandraClientOptions().addContactPoint("localhost")
 				.setKeyspace("desafio");
-		;
 		cassandraClient = CassandraClient.createShared(vertx, options);
 
 		Router router = Router.router(vertx);
 		router.route("/api/planeta*").handler(BodyHandler.create());
 		router.get("/api/planetas").handler(this::getPlanetas);
 		router.post("/api/planeta").handler(this::addPlaneta);
-		// router.get("/api/planetas/swapi").handler(this::getPlanetasFromAPI);
-		router.get("/api/planetas/:id").handler(this::getPlanetasById);
+		router.get("/api/planetas/swapi").handler(this::getPlanetasFromAPI);
+		router.get("/api/planeta/:id").handler(this::getPlanetaById);
+		router.delete("/api/planeta/:id").handler(this::deletePlaneta);
 
 		vertx.createHttpServer().requestHandler(router).listen(8080);
+
 	}
 
 	private void addPlaneta(RoutingContext rc) {
@@ -67,27 +80,44 @@ public class StarWarsService extends AbstractVerticle {
 					t.printStackTrace();
 					response.setStatusCode(500).end("Unable to execute the query");
 				}, () -> {
-					response.putHeader("content-type", "application/json;charset=utf-8")
-							.end(Json.encodePrettily(planetas));
+					String nome = rc.request().getParam("nome");
+					if (nome != null && !nome.isEmpty()) {
+						response.putHeader("content-type", "application/json;charset=utf-8")
+								.end(Json.encodePrettily(
+										planetas.stream().filter(predicate -> predicate.getNome().equals(nome))
+												.collect(Collectors.toList())));
+					} else {
+						response.putHeader("content-type", "application/json;charset=utf-8")
+								.end(Json.encodePrettily(planetas));
+					}
 				});
 	}
 
-	/*
-	 * private void getPlanetasFromAPI(RoutingContext rc) {
-	 * 
-	 * client = WebClient.create(vertx);
-	 * 
-	 * HttpRequest<JsonObject> request = client.get(443,
-	 * "https://swapi.co/api/planets", "") .putHeader("Accept",
-	 * "application/json").as(BodyCodec.jsonObject()); request.send(ar -> { if
-	 * (ar.failed()) { rc.fail(ar.cause()); } else {
-	 * rc.response().putHeader(HttpHeaders.CONTENT_TYPE,
-	 * "application/json").end(ar.result().body().encode()); } });
-	 * 
-	 * }
-	 */
+	private void getPlanetasFromAPI(RoutingContext rc) {
 
-	private void getPlanetasById(RoutingContext rc) {
+		WebClientOptions options = new WebClientOptions().setKeepAlive(false);
+		WebClient client = WebClient.create(vertx, options);
+
+		client.getAbs("https://swapi.co/api/planets").send(ar -> {
+			if (ar.succeeded()) {
+				HttpResponse<Buffer> response = ar.result();
+				rc.response().putHeader(HttpHeaders.CONTENT_TYPE,"application/json").end(response.body());
+			} else {
+				System.out.println("Something went wrong " + ar.cause().getMessage());
+			}
+		});
+		// Single<HttpResponse<JsonObject>> single =
+		// client.get(443,"https://swapi.co","/api/planets")
+		// .putHeader(HttpHeaders.ACCEPT.toString(),
+		// "application/json").as(BodyCodec.jsonObject())
+		// .rxSend(); .as(BodyCodec.jsonObject()).rxSend();
+		// single.subscribe(resp -> {
+		// rc.response().putHeader(HttpHeaders.CONTENT_TYPE,
+		// "application/json").end(resp.body().encode());
+		// });
+	}
+
+	private void getPlanetaById(RoutingContext rc) {
 
 		int id = Integer.parseInt(rc.request().getParam("id"));
 
@@ -103,27 +133,19 @@ public class StarWarsService extends AbstractVerticle {
 
 	}
 
-	private void getPlanetasByNome(RoutingContext rc) {
+	private void deletePlaneta(RoutingContext rc) {
+
+		int id = Integer.parseInt(rc.request().getParam("id"));
 
 		HttpServerResponse response = rc.response();
 
-		String nome = rc.request().getParam("nome");
+		MappingManager mappingManager = MappingManager.create(cassandraClient);
+		Mapper<Planeta> mapper = mappingManager.mapper(Planeta.class);
 
-		System.out.println(nome);
-
-		List<Planeta> planetas = new ArrayList<Planeta>();
-
-		cassandraClient.rxQueryStream("SELECT * FROM planetas WHERE nome = " + nome).flatMapPublisher(CassandraRowStream::toFlowable)
-				.subscribe(row -> {
-					planetas.add(new Planeta(row.getInt("id"), row.getString("nome"), row.getString("clima"),
-							row.getString("terreno")));
-				}, t -> {
-					t.printStackTrace();
-					response.setStatusCode(500).end("Unable to execute the query");
-				}, () -> {
-					response.putHeader("content-type", "application/json;charset=utf-8")
-							.end(Json.encodePrettily(planetas));
-				});
+		mapper.delete(Collections.singletonList(id), handler -> {
+			response.putHeader("content-type", "application/json; charset=utf-8")
+					.end(Json.encodePrettily(handler.result()));
+		});
 
 	}
 }
